fd0ebcf2a5b2ba07a37f701be16099166665d645
Charllys Emerenciano <charllysemerenciano@gmail.com>
2020-07-14 22:07:03 -0300

    Commit inicial dos arquivos do projeto

---
+++ src/test/java/com/acme/lfapi/LfapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.lfapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LfapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
